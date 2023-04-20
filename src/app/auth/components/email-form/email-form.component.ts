f717acb24e06b1d0acbcda72e8c014ca8df8f297
K Rob <OctMoon@oct-moon.lan>
2023-04-20 16:17:52 -0700

    :sparkles: email-form html base styles and functionality added to app

--- src/app/auth/components/email-form/email-form.component.ts
+++ src/app/auth/components/email-form/email-form.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
+import { NgForm } from '@angular/forms'
+
+export interface EmailLoginForm {
+	name: string
+	email: string
+	password: string
+	agreeToTerms: boolean
+	rememberMe: boolean
+}
 
 @Component({
-  selector: 'app-email-form',
-  templateUrl: './email-form.component.html',
-  styleUrls: ['./email-form.component.scss']
+	selector: 'email-form',
+	templateUrl: './email-form.component.html',
+	styleUrls: ['./email-form.component.scss'],
 })
-export class EmailFormComponent {
+export class EmailFormComponent implements OnInit {
+	@Output() formSubmit = new EventEmitter<EmailLoginForm>()
 
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	type = 'login'
+
+	changeType(val: any) {
+		this.type = val
+	}
+
+	get isLogin() {
+		return this.type === 'login'
+	}
+
+	get isSignup() {
+		return this.type === 'signup'
+	}
+	handleSubmit(form: NgForm) {
+		if (form.valid) {
+			this.formSubmit.emit(form.value)
+			form.resetForm()
+		} else {
+			form.form.markAllAsTouched()
+		}
+	}
 }
--- src/app/auth/containers/login-page/login-page.component.ts
+++ src/app/auth/containers/login-page/login-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { EmailLoginForm } from '../../components/email-form/email-form.component'
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+	selector: 'app-login-page',
+	templateUrl: './login-page.component.html',
+	styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
+	loginWithGoogle() {
+		console.log('Login with Google!')
+	}
+	loginWithFacebook() {
+		console.log('Login with Facebook!')
+	}
+	loginWithTwitter() {
+		console.log('Login with Twitter!')
+	}
+	loginWithGithub() {
+		console.log('Login with Github!')
+	}
 
+	handleEmailLoginForm(data: EmailLoginForm) {
+		console.log('Email login form data:', data)
+		// Process the form data as needed
+	}
 }
