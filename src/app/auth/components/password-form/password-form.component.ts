09576f469db99ddfe410a046286f1606be0e1a07
K Rob <OctMoon@oct-moon.lan>
2023-04-20 16:29:02 -0700

    :art: forgot-password.component content added and sytled

--- src/app/auth/components/password-form/password-form.component.ts
+++ src/app/auth/components/password-form/password-form.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-password-form',
-  templateUrl: './password-form.component.html',
-  styleUrls: ['./password-form.component.scss']
+	selector: 'password-form',
+	templateUrl: './password-form.component.html',
+	styleUrls: ['./password-form.component.scss'],
 })
-export class PasswordFormComponent {
-
-}
+export class PasswordFormComponent {}
--- src/app/auth/containers/forgot-password-page/forgot-password-page.component.ts
+++ src/app/auth/containers/forgot-password-page/forgot-password-page.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-forgot-password-page',
-  templateUrl: './forgot-password-page.component.html',
-  styleUrls: ['./forgot-password-page.component.scss']
+	selector: 'forgot-password-page',
+	templateUrl: './forgot-password-page.component.html',
+	styleUrls: ['./forgot-password-page.component.scss'],
 })
-export class ForgotPasswordPageComponent {
-
-}
+export class ForgotPasswordPageComponent {}
