3744e5ee5ab13ac02b3035d1153c82e5014b2217
K Rob <OctMoon@oct-moon.lan>
2023-04-20 13:13:31 -0700

    :sparkles: updated auth.routing to make login the default

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,33 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-import { LoginPageComponent } from './containers/login-page/login-page.component';
-import { ForgotPasswordPageComponent } from "./containers/forgot-password-page/forgot-password-page.component";
-import { RegisterPageComponent } from "./containers/register-page/register-page.component";
+import { LoginPageComponent } from './containers/login-page/login-page.component'
+import { ForgotPasswordPageComponent } from './containers/forgot-password-page/forgot-password-page.component'
+import { RegisterPageComponent } from './containers/register-page/register-page.component'
 
 const routes: Routes = [
-  {
-    path: '', // Default route to the login page
-    children: [
-      {
-        path: 'login', // Redirect to the login page
-        component: LoginPageComponent,
-      },
-      {
-        path: 'register', // Redirect to the login page
-        component: RegisterPageComponent,
-      },
-      {
-        path: 'forgot-password', // Redirect to the login page
-        component: ForgotPasswordPageComponent,
-      },
-      // Add other child routes here as needed
-    ],
-  },
-];
+	{
+		path: '', // Default route to the login page
+		children: [
+			{
+				path: 'login', // Redirect to the login page
+				component: LoginPageComponent,
+			},
+			{
+				path: 'register', // Redirect to the login page
+				component: RegisterPageComponent,
+			},
+			{
+				path: 'forgot-password', // Redirect to the login page
+				component: ForgotPasswordPageComponent,
+			},
+			// Add other child routes here as needed
+		],
+	},
+	{ path: '', redirectTo: 'login', pathMatch: 'full' },
+]
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule],
 })
 export class AuthRoutingModule {}
