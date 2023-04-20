eb3678e04083aad70a1492f80bdfaa51c5a65e92
K Rob <OctMoon@oct-moon.lan>
2023-04-20 13:08:02 -0700

    :art: top-bar added to site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { AuthGuard } from "./guards/auth.guard";
+import { TopBarComponent } from './shared/components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
